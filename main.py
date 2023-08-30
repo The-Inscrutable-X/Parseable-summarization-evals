44a87fa42c1bd59af314ee5a2075341d25bca935
Hanqi Xiao <hanqixiao.personal@gmail.com>
2023-08-30 13:54:25 -0400

    Initial system for sending requests to OPENAI api, and function for parsing sentences from blocks of text

---
+++ main.py
@@ -0,0 +1,3 @@
+# The purpose of this main file is to run the evaluations for generating very structured parseable completions that summarizes specific variable relations
+# from academic papers. This evaluation is dependent on the assumption that vocabulary usage in peer reviewed papers are CONSISTENT. 
+
